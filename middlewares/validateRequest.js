007213bbca759895ccf5003b51e168bdddac4216
hexzoner <hexzon@gmail.com>
2024-07-03 22:45:56 +0200

    first commit

---
+++ middlewares/validateRequest.js
@@ -0,0 +1,32 @@
+import { userSchema } from "../schemas/users.js";
+import { eventSchema } from "../schemas/events.js";
+import { ErrorResponse } from "../utils/ErrorResponse.js";
+
+export const validateRequest = (req, res, next) => {
+  const {
+    params: { model },
+    method,
+  } = req;
+  let schema;
+
+  switch (model) {
+    case "users":
+      schema = userSchema[method];
+      break;
+    case "events":
+      schema = eventSchema[method];
+      break;
+    default:
+      return next(new ErrorResponse("Invalid model specified", 404));
+  }
+
+  const { error } = schema?.validate(req.body);
+  if (error) return next(new ErrorResponse(error, 400));
+  next();
+};
+
+export const validateUser = (req, res, next) => {
+  const { error } = userSchema.POST.validate(req.body);
+  if (error) return next(new ErrorResponse(error, 400));
+  next();
+};
